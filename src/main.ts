ddf15f5f28b29a771cab5cae44ca0e3c83569997
Harshit <harshith.k@polynomial.ai>
2022-09-24 17:07:33 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {environment} from './environments/environment';
 
-import {AppModule} from './app/6FigureExpertAdmin/app.module';
-// import {AppModule} from './app/6FigureExpert/app.module';
+// import {AppModule} from './app/6FigureExpertAdmin/app.module';
+import {AppModule} from './app/6FigureExpert/app.module';
 
 
 if (environment.production)
